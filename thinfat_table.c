2af9124a96887d16967d474ea16fb965eeee7b3e
Hiroka Ihara <ihara_h@hongotechgarage.jp>
2017-01-17 00:24:25 +0900

    Implemented some TBL layer functions

--- thinfat_table.c
+++ thinfat_table.c
@@ -55,6 +55,8 @@ thinfat_result_t thinfat_table_seek(void *client, thinfat_table_t *table, thinfa
 }
 
 static thinfat_result_t thinfat_table_search_read_callback(thinfat_table_t *table, thinfat_sector_t s_param, void *p_param);
+static thinfat_result_t thinfat_table_create_chain_callback(thinfat_table_t *table, thinfat_sector_t s_param, void *p_param);
+static thinfat_result_t thinfat_table_deallocate_callback(thinfat_table_t *table, thinfat_sector_t s_param, void *p_param);
 
 thinfat_result_t thinfat_table_callback(thinfat_table_t *table, thinfat_core_event_t event, thinfat_sector_t s_param, void *p_param)
 {
@@ -86,15 +88,60 @@ thinfat_result_t thinfat_table_callback(thinfat_table_t *table, thinfat_core_eve
     return thinfat_table_search_read_callback(table, s_param, p_param);
   case THINFAT_TABLE_EVENT_SEARCH_FOUND:
     THINFAT_INFO("Cluster found @ " TFF_X32 " * " TFF_U32 "\n", *(thinfat_cluster_t *)p_param, table->cc_search);
-    return thinfat_core_callback(table->client, table->event, THINFAT_INVALID_SECTOR, NULL);
+    table->ci_to = *(thinfat_cluster_t *)p_param + table->cc_search;
+    table->ci_from = *(thinfat_cluster_t *)p_param;
+    return thinfat_cached_read_single(table, table->cache, tf->si_hidden + tf->sc_reserved + *(thinfat_cluster_t *)p_param / (THINFAT_SECTOR_SIZE >> tf->type), THINFAT_TABLE_EVENT_CREATE_CHAIN_READ);
+    //return thinfat_core_callback(table->client, table->event, THINFAT_INVALID_SECTOR, NULL);
   case THINFAT_TABLE_EVENT_CONCATENATE_READ:
     if (tf->type == THINFAT_TYPE_FAT32)
       thinfat_write_u32(*(void **)p_param, (table->ci_from % (THINFAT_SECTOR_SIZE / 4)) * 4, table->ci_to);
     else
       thinfat_write_u16(*(void **)p_param, (table->ci_from % (THINFAT_SECTOR_SIZE / 2)) * 2, table->ci_to);
+    thinfat_cache_touch(table->cache);
     return thinfat_core_callback(table->client, table->event, THINFAT_INVALID_SECTOR, NULL);
+  case THINFAT_TABLE_EVENT_CREATE_CHAIN_READ:
+    return thinfat_table_create_chain_callback(table, s_param, p_param);
+  case THINFAT_TABLE_EVENT_DEALLOCATE_READ:
+    return thinfat_table_deallocate_callback(table, s_param, p_param);
   }
   return THINFAT_RESULT_OK;
+}
+
+static thinfat_result_t thinfat_table_create_chain_callback(thinfat_table_t *table, thinfat_sector_t s_param, void *p_param)
+{
+  thinfat_t *tf = (thinfat_t *)table->parent;
+  unsigned int i = table->ci_from % (THINFAT_SECTOR_SIZE >> tf->type);
+  thinfat_cluster_t ci_start = (s_param - tf->si_hidden - tf->sc_reserved) * (THINFAT_SECTOR_SIZE >> tf->type);
+  for (; i < THINFAT_SECTOR_SIZE >> tf->type; i++)
+  {
+    if (ci_start + i + 1 == table->ci_to)
+    {
+      if (tf->type == THINFAT_TYPE_FAT32)
+      {
+        uint32_t ci_value = thinfat_read_u32(*(void **)p_param, i * 4) & THINFAT_FAT32_CLUSTER_HIGH_MASK;
+        thinfat_write_u32(*(void **)p_param, i * 4, ci_value | THINFAT_FAT32_EOC);
+      }
+      else
+      {
+        thinfat_write_u16(*(void **)p_param, i * 2, 0xFFF8);
+      }
+      return thinfat_core_callback(table->client, table->event, THINFAT_INVALID_SECTOR, NULL);
+    }
+    else
+    {
+      if (tf->type == THINFAT_TYPE_FAT32)
+      {
+        uint32_t ci_value = thinfat_read_u32(*(void **)p_param, i * 4) & THINFAT_FAT32_CLUSTER_HIGH_MASK;
+        thinfat_write_u32(*(void **)p_param, i * 4, ci_value | (ci_start + i + 1));
+      }
+      else
+      {
+        thinfat_write_u16(*(void **)p_param, i * 2, ci_start + i + 1);
+      }
+    }
+  }
+  table->ci_from = (table->ci_from + (THINFAT_SECTOR_SIZE >> tf->type)) / (THINFAT_SECTOR_SIZE >> tf->type) * (THINFAT_SECTOR_SIZE >> tf->type);
+  return thinfat_cached_read_single(table, table->cache, s_param + 1, THINFAT_TABLE_EVENT_CREATE_CHAIN_READ);
 }
 
 static thinfat_result_t thinfat_table_search_read_callback(thinfat_table_t *table, thinfat_sector_t si_read, void *p_param)
@@ -132,6 +179,31 @@ static thinfat_result_t thinfat_table_search_read_callback(thinfat_table_t *tabl
     return thinfat_core_callback(table->client, table->event, THINFAT_INVALID_SECTOR, NULL);
 }
 
+static thinfat_result_t thinfat_table_deallocate_callback(thinfat_table_t *table, thinfat_sector_t s_param, void *p_param)
+{
+  thinfat_t *tf = (thinfat_t *)table->parent;
+  unsigned int i = table->ci_from % (THINFAT_SECTOR_SIZE >> tf->type);
+  thinfat_cluster_t ci_start = (s_param - tf->si_hidden - tf->sc_reserved) * (THINFAT_SECTOR_SIZE >> tf->type);
+
+  for (; i < THINFAT_SECTOR_SIZE >> tf->type; i++)
+  {
+    if (tf->type == THINFAT_TYPE_FAT32)
+    {
+      uint32_t ci_value = thinfat_read_u32(*(void **)p_param, i * 4) & THINFAT_FAT32_CLUSTER_HIGH_MASK;
+      thinfat_write_u32(*(void **)p_param, i * 4, ci_value);
+    }
+    else
+    {
+      thinfat_write_u16(*(void **)p_param, i * 2, 0);
+    }
+    if (table->ci_from + i + 1 == table->ci_to)
+    {
+      return thinfat_core_callback(table->client, table->event, THINFAT_INVALID_SECTOR, NULL);
+    }
+  }
+  return thinfat_cached_read_single(table, table->cache, s_param + 1, THINFAT_TABLE_EVENT_DEALLOCATE_READ);
+}
+
 thinfat_result_t thinfat_table_init(thinfat_table_t *table, thinfat_t *parent)
 {
   table->parent = parent;
@@ -145,14 +217,13 @@ static thinfat_result_t thinfat_table_search(thinfat_table_t *table, thinfat_clu
 {
   thinfat_t *tf = (thinfat_t *)table->parent;
 
-  thinfat_cluster_t cc_current = 0;
-  thinfat_cluster_t ci_current = tf->ci_next_free;
-  if (!THINFAT_IS_CLUSTER_VALID(ci_current))
-    ci_current = 2;
+  thinfat_cluster_t ci_initial = tf->ci_next_free;
+  if (!THINFAT_IS_CLUSTER_VALID(ci_initial))
+    ci_initial = 2;
   
   thinfat_cluster_t cc_total = tf->si_hidden + tf->sc_volume_size - tf->si_data;
 
-  thinfat_sector_t si_table = tf->si_hidden + tf->sc_reserved + ci_current / (THINFAT_SECTOR_SIZE >> tf->type);
+  thinfat_sector_t si_table = tf->si_hidden + tf->sc_reserved + ci_initial / (THINFAT_SECTOR_SIZE >> tf->type);
 
   table->cc_search = cc_search;
   table->cc_search_count = 0;
@@ -174,10 +245,14 @@ thinfat_result_t thinfat_table_deallocate(void *client, thinfat_table_t *table, 
 {
   thinfat_t *tf = (thinfat_t *)table->parent;
 
+  thinfat_sector_t si_read = tf->si_hidden + tf->sc_reserved + ci_deallocate / (THINFAT_SECTOR_SIZE >> tf->type);
+
   table->client = client;
   table->event = event;
+  table->ci_from = ci_deallocate;
+  table->ci_to = ci_deallocate + cc_deallocate;
 
-  return THINFAT_RESULT_UNSUPPORTED;
+  return thinfat_cached_read_single(table, table->cache, si_read, THINFAT_TABLE_EVENT_DEALLOCATE_READ);
 }
 
 thinfat_result_t thinfat_table_concatenate(void *client, thinfat_table_t *table, thinfat_cluster_t ci_from, thinfat_cluster_t ci_to, thinfat_core_event_t event)
